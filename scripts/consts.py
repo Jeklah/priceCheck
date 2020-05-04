75e6e14a4787f57e3ae06b5667019fbc8ed8638c
Jeklah <arthurbowers@gmail.com>
2020-05-04 22:00:54 +0100

    Refactoring consts into a seperate file.

---
+++ scripts/consts.py
@@ -0,0 +1,39 @@
+oreList = [
+            'Tritanium',
+            'Pyerite',
+            'Isogen',
+            'Megacyte',
+            'Mexallon',
+            'Nocxium',
+            'Zydrine'
+           ]
+
+marketList = [
+                'jita',
+                'hek',
+                'amarr',
+                'rens',
+                'dodixie'
+              ]
+
+partsList = [
+                'Capital Capacitor Battery',
+                'Capital Cargo Bay',
+                'Capital Computer System',
+                'Capital Construction Parts',
+                'Capital Corporate Hangar Bay',
+                'Capital Sensor Cluster',
+                'Capital Ship Maintenance Bay'
+            ]
+
+shipPartCounts = {
+                    'Orca':{
+                        '9',
+                        '35',
+                        '7',
+                        '15',
+                        '4',
+                        '4',
+                        '7'
+                    }
+                 }
