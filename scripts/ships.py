63c36cce5bb71db895285bd34e830574a2552aa2
Jeklah <arthurbowers@gmail.com>
2020-09-27 03:36:20 +0100

    spacing typo

--- scripts/ships.py
+++ scripts/ships.py
@@ -133,7 +133,7 @@ def main(single, market, compare):
         for mrkt in marketList:
             partDetails = get_appraisal(compare, mrkt)
             cost = round(partDetails[minPrice], 2)
-            click.echo(compare.capitalize() + ' costs ' + '{:,}'.format(cost) + 'ISK at ' + mrkt.capitalize())
+            click.echo(compare.capitalize() + ' costs ' + '{:,}'.format(cost) + ' ISK at ' + mrkt.capitalize())
     elif market and not single:
         market_check(market)
         shipName = choose_ship()
